926caa0099e4f2dc84e0f8745606793167e04e2b
-s8 <117573563+s8seer@users.noreply.github.com>
2023-04-15 02:32:51 +0300

    Delete .config directory

--- .config/awesome/scripts/scropy2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrot -s  -l color="#92f86d",width=3,mode=edge,opacity=50 -e  'xclip -selection clip-board -t image/png $f ; rm $f'
-
-# Selection area scerenshot with scrot.
-# Selected region screenshot is saved in the HOME directory and deleted instantly afterwards.
-# mode=MODE, MODE is either "classic" or "edge". "classic" is deprecated and troublesome.
