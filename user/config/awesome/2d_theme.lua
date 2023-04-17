5a4088e1694c7c51497e053896b039bca98b0b69
-s8 <117573563+s8seer@users.noreply.github.com>
2023-04-17 03:40:41 +0300

    removed menu icons due to menu overhaul

--- user/config/awesome/2d_theme.lua
+++ user/config/awesome/2d_theme.lua
@@ -20,13 +20,13 @@ sexy.wallpaper = THEME_FOLDER.."/wallpaper"
 
 sexy.bg_normal     = "#1A2024"
 sexy.bg_focus      = "#00c4b0"
-sexy.bg_minimize   = "#345351"
+sexy.bg_minimize   = "#444444"
 sexy.bg_urgent     = "#ffaebc"
 
 sexy.fg_normal     = "#FCF1E3"
 sexy.fg_focus      = "#FCF1E3"
 sexy.fg_urgent     = "#1A2024"
-sexy.fg_minimize   = "#FCF1E3"
+sexy.fg_minimize   = "#1A2024"
 
 sexy.useless_gap   = dpi(8)
 sexy.border_width  = dpi(3)
@@ -65,8 +65,8 @@ sexy.hotkeys_fg = "#aaaaaa"
 sexy.hotkeys_label_fg = sexy.hotkeys_bg
 
 
-sexy.menu_height = dpi(34)
-sexy.menu_width  = dpi(257)
+sexy.menu_height = dpi(26)
+sexy.menu_width  = dpi(220)
 sexy.menu_border_width = dpi(2)
 sexy.menu_border_color = "#fad02c"
 sexy.menu_bg_normal = "#1A2024"
@@ -75,8 +75,6 @@ sexy.menu_fg_normal = "#FCF1E3"
 sexy.menu_fg_focus = "#1A2024"
 
 sexy.wibar_height=28
-
-
 
 -- You can use your own layout icons like this:
 
@@ -97,28 +95,9 @@ sexy.layout_dwindle = layout_path.."dwindlew.png"
 sexy.awesome_icon = icon_path.."draw_arch.png"
 sexy.menu_submenu_icon = icon_path.."draw_expand.png"
 
--- Games
-sexy.games_icon = icon_path.."games.png"
-sexy.steam_icon = icon_path.."steam.png"
-sexy.osu_icon = icon_path.."game_osu.png"
-sexy.minecraft_icon = icon_path.."game_minecraft.png"
-
--- Apps
-sexy.apps_icon = icon_path.."app_firefox.png"
-sexy.firefox_icon = icon_path.."app_firefox.png"
-
--- Social
-sexy.social_icon = icon_path.."soc_social.png"
-sexy.telegram_icon = icon_path.."telegram.png"
-sexy.discord_icon = icon_path.."soc_discord.png"
-
--- Programs
-sexy.programs_icon = icon_path.."prog_program.png"
-sexy.files_icon = icon_path.."prog_file.png" 
-sexy.code_icon = icon_path.."prog_code.png"
-
--- Computer
+-- Entries
 sexy.computer_icon = icon_path.."com_computer.png"
+sexy.firefox_icon = icon_path.."app_firefox.png"
 sexy.question_icon = icon_path.."com_info.png"
 sexy.reload_icon = icon_path.."com_reload.png"
 sexy.logout_icon = icon_path.."com_logout.png"
